a304dea136a76bfc236ddd97a3b2fd5c8bfcd47e
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2023-08-03 19:20:34 +0200

    Update and rename papirus.sh to papirus_+_papirus_folder.sh

--- papirus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo add-apt-repository ppa:papirus/papirus
-sudo apt-get update
-sudo apt-get install papirus-icon-theme papirus-folders -y
-
-papirus-folders -C yaru --theme Papirus-Dark
