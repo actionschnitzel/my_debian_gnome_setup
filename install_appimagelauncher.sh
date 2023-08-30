3f3264c3ec2711265ec291035e3a227973bc703c
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2023-08-30 14:27:41 +0200

    Create install_appimagelauncher.sh

---
+++ install_appimagelauncher.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Install appimagelauncher
+wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb
+
+sudo dpkg -i appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb
