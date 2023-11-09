b0cc527877169c764a443f3eb2783e2e54d0ebc5
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2023-11-10 00:41:26 +0100

    Update flatpaks_favorite_install.sh

--- flatpaks_favorite_install.sh
+++ flatpaks_favorite_install.sh
@@ -18,10 +18,11 @@ flatpak install flathub org.telegram.desktop -y
 flatpak install flathub com.discordapp.Discord -y
 
 #Fluent Reader
-flatpak install flathub me.hyliu.fluentreader -y
+flatpak install fio.gitlab.news_flash.NewsFlash -y
 
 #Nextcloud
 flatpak install flathub com.nextcloud.desktopclient.nextcloud -y
 
 #Flatsweep
 flatpak install flathub io.github.giantpinkrobots.flatsweep -y
+
