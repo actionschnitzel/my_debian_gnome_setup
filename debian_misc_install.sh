6fec535294ab4d59d6db32ee0ad4ddfcce62ef66
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2023-11-10 00:47:33 +0100

    Update debian_misc_install.sh

--- debian_misc_install.sh
+++ debian_misc_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install synaptic tilix bpytop vlc gparted audacity easytag thonny 2048-qt paperwork-gtk git 
+sudo apt update && sudo apt install synaptic tilix bpytop vlc gparted audacity easytag thonny 2048-qt paperwork-gtk git nala
