5f909aec6823f305866c534d286516c26db1aa96
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2023-11-10 13:53:44 +0100

    Create my_dev_env.sh

---
+++ my_dev_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#My DEV Environment
+
+sudo apt-get install xterm python3-pil python3-pil.imagetk python3-pip python3-psutil python3-distro python3-bs4 python3-dev python3-requests mpg123 lolcat wmctrl gdebi mousepad autoflake
