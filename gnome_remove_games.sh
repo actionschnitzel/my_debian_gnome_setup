d9df9e5b559f0b19e2193dc235b8aa9139b8b629
Timo Westphal <67635013+actionschnitzel@users.noreply.github.com>
2024-01-17 02:25:06 +0100

    ä

---
+++ gnome_remove_games.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Remove gnome games
+sudo apt purge iagno lightsoff four-in-a-row gnome-robots pegsolitaire gnome-2048 hitori gnome-klotski gnome-mines gnome-mahjongg gnome-sudoku quadrapassel swell-foop gnome-tetravex gnome-taquin aisleriot gnome-chess five-or-more gnome-nibbles tali && sudo apt autoremove
