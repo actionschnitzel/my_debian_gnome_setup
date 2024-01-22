2ef32d9abb43e2e7e3fc74c75df3f21ee493bd17
Actionschnitzel <67635013+actionschnitzel@users.noreply.github.com>
2024-01-22 13:42:59 +0100

    Create install_lutris.sh

---
+++ install_lutris.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "deb [signed-by=/etc/apt/keyrings/lutris.gpg] https://download.opensuse.org/repositories/home:/strycore/Debian_12/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list > /dev/null
+wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/keyrings/lutris.gpg > /dev/null
+sudo apt update
+sudo apt install lutris -y
