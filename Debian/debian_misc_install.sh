3177884cf623326b9a55c2ce9292b40ffe0ceeb9
Actionschnitzel <67635013+actionschnitzel@users.noreply.github.com>
2024-01-22 02:55:59 +0100

    Update debian_misc_install.sh

--- Debian/debian_misc_install.sh
+++ Debian/debian_misc_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install synaptic tilix bpytop vlc gparted audacity easytag thonny paperwork-gtk git nala thunderbird gnote
+sudo apt update && sudo apt install synaptic tilix bpytop vlc gparted audacity easytag thonny paperwork-gtk git nala thunderbird gnote gnome-shell-extension-alphabetical-grid gnome-shell-extension-appindicator gnome-shell-extension-manager
